512162d30c277b836797d8bfedf09044886bc13e
Ayush Karma <ayushkarma.dev@gmail.com>
2025-08-05 21:32:11 +0530

    added middleware, also work on some routing

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,13 +1,36 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Navbar from "../nav/page";
+import axios from "axios";
+import Link from "next/link";
 
 function ProfilePage() {
+  const [data, setData] = useState("Nothing");
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
   return (
     <>
       <Navbar />
       <div className="flex items-center justify-center h-[100vh]">
-        Profile page
+        Profile page <br />
         <h1 className="4xl"></h1>
+        <h2>
+          {data === "Nothing" ? (
+            "Nothing"
+          ) : (
+            <Link href={`/profile/${data}`}>{data}</Link>
+          )}
+        </h2>
+        <br />
+        <button
+          className="bg-blue-500 px-4 py-2 rounded-lg"
+          onClick={getUserDetails}
+        >
+          Get User details
+        </button>
       </div>
     </>
   );
